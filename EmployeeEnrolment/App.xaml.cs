15bface57b7bf401b7173c7f39ec022cf6512399
Thomas Underwood <37879441+wraig0@users.noreply.github.com>
2024-07-11 10:42:24 +0100

    init

---
+++ EmployeeEnrolment/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeEnrolment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
